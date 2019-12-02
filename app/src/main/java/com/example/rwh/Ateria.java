93bdb793a026c9a4111bcdc3c51f0d1708a9f26e
kristianmetro <55086950+kristianmetro@users.noreply.github.com>
2019-12-02 14:48:54 +0200

    Toimiva versio

---
+++ app/src/main/java/com/example/rwh/Ateria.java
@@ -0,0 +1,45 @@
+package com.example.rwh;
+
+import java.util.ArrayList;
+
+public class Ateria {
+    private String aterianNimi;
+    private ArrayList<String> ruoat;
+
+    public Ateria(String nimi, ArrayList<String> ruoat){
+        this.aterianNimi = nimi;
+        this.ruoat = ruoat;
+    }
+
+    public String getAterianNimi() {
+        return aterianNimi;
+    }
+
+    public void setAterianNimi(String aterianNimi) {
+        this.aterianNimi = aterianNimi;
+    }
+
+    public ArrayList<String> getRuoat() {
+        return ruoat;
+    }
+
+    public void setRuoat(ArrayList<String> ruoat) {
+        this.ruoat = ruoat;
+    }
+
+    /*@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ateria ateria = (Ateria) o;
+        return aterianNimi.equals(ateria.aterianNimi) &&
+                ruoat.equals(ateria.ruoat);
+    }*/
+
+    /*@Override
+    public String toString(){
+        return this.pvm;
+    }*/
+}
+
+
