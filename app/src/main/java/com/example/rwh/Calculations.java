9aec2f711e4d9602544219ffc73637fbafc769b8
TheSovietTerror <ludwigdmitri@yahoo.com>
2019-12-03 11:17:59 +0200

    Added calculations.java data from spinners to calculate burn calories

---
+++ app/src/main/java/com/example/rwh/Calculations.java
@@ -0,0 +1,80 @@
+package com.example.rwh;
+import java.lang.*;
+
+public class Calculations {
+    int aika;
+    public double sport(String sport, String time,int met) {
+        if (sport.equals("Kävely")) {
+            if (time.equals("10 min")) {
+                aika = 10;
+            } else if (time.equals("20 min")) {
+                aika = 20;
+            } else if (time.equals("30 min")) {
+                aika = 30;
+            } else if (time.equals("40 min")) {
+                aika = 40;
+            } else if (time.equals("50 min")) {
+                aika = 50;
+            } else if (time.equals("60 min")) {
+                aika = 60;
+            }
+            //2.5 met	bird watching, slow walk
+            return (((met * 2.5) / 6) * aika);
+
+        } else if (sport.equals("Hölkkä")) {
+            if (time.equals("10 min")) {
+                aika = 10;
+            } else if (time.equals("20 min")) {
+                aika = 20;
+            } else if (time.equals("30 min")) {
+                aika = 30;
+            } else if (time.equals("40 min")) {
+                aika = 40;
+            } else if (time.equals("50 min")) {
+                aika = 50;
+            } else if (time.equals("60 min")) {
+                aika = 60;
+            }
+            //7.0 met	jogging, general
+            return (((met * 7.0) / 6) * aika);
+        } else if (sport.equals("Juoksu")) {
+            if (time.equals("10 min")) {
+                aika = 10;
+            } else if (time.equals("20 min")) {
+                aika = 20;
+            } else if (time.equals("30 min")) {
+                aika = 30;
+            } else if (time.equals("40 min")) {
+                aika = 40;
+            } else if (time.equals("50 min")) {
+                aika = 50;
+            } else if (time.equals("60 min")) {
+                aika = 60;
+            }
+            //9.8	running, 6 mph (10 min/mile)
+            return (((met * 9.8) / 6) * aika);
+        } else if (sport.equals("PikaJuoksu")) {
+            if (time.equals("10 min")) {
+                aika = 10;
+            } else if (time.equals("20 min")) {
+                aika = 20;
+            } else if (time.equals("30 min")) {
+                aika = 30;
+            } else if (time.equals("40 min")) {
+                aika = 40;
+            } else if (time.equals("50 min")) {
+                aika = 50;
+            } else if (time.equals("60 min")) {
+                aika = 60;
+            }
+            // 19.0
+            //running, 12 mph (5 min/mile)
+            return (((met * 19.0) / 6) * aika);
+        } else
+        {
+            return 0;
+        }
+
+
+    }
+}
